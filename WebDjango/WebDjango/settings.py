808425b392ab69c35e3436d5a906231b426ab95e
Preent Loo <psilocybit5211@gmail.com>
2021-11-11 22:24:52 -0300

    Se agrega cometario al archivo settings.py

--- WebDjango/WebDjango/settings.py
+++ WebDjango/WebDjango/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# DJango buscara en esta ruta, los archivos estaticos que necesite
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
